3fc08f6fb4c7be83154411268ab7369827b029da
Jerry Sandhu <jsa2282@wgu.edu>
2024-05-14 21:02:17 -0700

    create unencrypted user/pass file

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 pwd = input("What is your master password? ")
 
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", passw)
+
+
+def add():
+    name = input("What is the name of the account? ")
+    pwd = input("What is the password? ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + " | " + pwd + "\n")
+
+
 while True:
-    mode = input("Would you like to add a new password or retrieve an existing one (view, add)(q to quit)? ").lower
+    mode = input("Would you like to add a new password or retrieve an existing one (view, add)(q to quit)? ")
+    mode.lower()
     if mode == "q":
         break
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode selected.")
         continue
